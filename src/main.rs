3c97175dfe01bbb01889186e1050ffd07f3db131
hayasshi <daisuke0884@gmail.com>
2021-05-24 22:31:11 +0900

    Add Clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{ArgSettings, Clap};
+
 fn main() {
-    println!("Hello World!");
+    let opts = Opts::parse();
+    println!("{:#?}", opts);
+}
+
+#[derive(Clap, Debug)]
+#[clap(name = "fourkeys-collector", version)]
+struct Opts {
+    /// 対象リポジトリ名 `org/repo`
+    repo: String,
+
+    /// GitHub personal access token. scope=repo:status
+    #[clap(long, env, setting = ArgSettings::HideEnvValues)]
+    github_token: String,
+
+    /// ベースブランチ
+    #[clap(short, long, default_value = "master")]
+    base: String,
+
+    /// 対象期間 from (JST) `yyyyMM`
+    #[clap(long)]
+    from: Option<String>,
+
+    /// 対象期間 to (JST) `yyyyMM`
+    #[clap(long)]
+    to: Option<String>,
 }
